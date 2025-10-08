24bc1bc2ce371638bc27460dbe4311f39304e960
esjay <esjaythegreat@gmail.com>
2025-10-08 22:52:52 +0900

    remove strapi admin auth warning

--- backend/config/admin.ts
+++ backend/config/admin.ts
@@ -1,6 +1,13 @@
 export default ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET'),
+    sessions: {
+      accessTokenLifespan: env.int('ADMIN_ACCESS_TTL', 1800),          // 30 min
+      maxRefreshTokenLifespan: env.int('ADMIN_REFRESH_TTL_MAX', 2592000), // 30 days
+      idleRefreshTokenLifespan: env.int('ADMIN_REFRESH_TTL_IDLE', 604800), // 7 days
+      maxSessionLifespan: env.int('ADMIN_SESSION_TTL_MAX', 2592000),      // 30 days
+      idleSessionLifespan: env.int('ADMIN_SESSION_TTL_IDLE', 3600),       // 1 hour
+    },
   },
   apiToken: {
     salt: env('API_TOKEN_SALT'),
